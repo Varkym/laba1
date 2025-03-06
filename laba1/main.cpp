fd91692179f84b3d85c20542b11b048d5e5a61c8
Варвара Сапегина <varvarasapegina@192.168.0.2>
2025-03-06 17:32:37 +0700

    Создание репозитория

---
+++ laba1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  laba1
+//
+//  Created by Варвара Сапегина on 06.03.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
