950b6c92e0ae12dc03cf477699ae823067673252
Варвара Сапегина <varvarasapegina@192.168.0.2>
2025-03-06 19:42:14 +0700

    Прямоугольник

--- laba1/main.cpp
+++ laba1/main.cpp
@@ -1,14 +1,28 @@
-//
-//  main.cpp
-//  laba1
-//
-//  Created by Варвара Сапегина on 06.03.2025.
-//
-
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+double Perimeter(double lenght, double widht) {
+    return (lenght + widht) * 2;
+}
+
+double Square(double lenght, double widht) {
+    return lenght * widht ;
+}
+
+double Diagonal(double light, double widht) {
+    return sqrt (pow (light, 2) + pow (widht, 2));
+}
+
+int main() {
+    double lenght = 5.5;
+    double widht = 3.0;
+    cout << "Длина прмоугольника: " << lenght << endl;
+    cout << "Ширина прямоугольника: " << widht << endl;
+    cout << "Периметр = " << Perimeter(lenght, widht ) << endl;
+    cout << "Пощадь = " << Square(lenght, widht) << endl;
+    cout << "Длина диагонали = " << Diagonal (lenght, widht) << endl;
     return 0;
+    
 }
